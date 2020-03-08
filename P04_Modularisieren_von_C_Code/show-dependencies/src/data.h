6d88ce49202ca8501a1ab6788379a6809c6e35d7
Andreas Gieriet <giei@zhaw.ch>
2020-03-08 06:26:31 +0100

    P04 lab code added

---
+++ P04_Modularisieren_von_C_Code/show-dependencies/src/data.h
@@ -0,0 +1,66 @@
+/**
+ * @file
+ * @brief  Access to the GCC produced dependency data (via gcc -H command line option).
+ */
+
+// begin of include guard
+// BEGIN-STUDENTS-TO-ADD-CODE
+
+
+// END-STUDENTS-TO-ADD-CODE
+
+
+// includes which are needed in this header file
+// BEGIN-STUDENTS-TO-ADD-CODE
+
+
+// END-STUDENTS-TO-ADD-CODE
+
+
+
+/**
+ * @brief  Directory container for file entries of the dependency file.
+ */
+// BEGIN-STUDENTS-TO-ADD-CODE
+
+
+// END-STUDENTS-TO-ADD-CODE
+
+
+/**
+ * @brief  File container for the file entries of the dependency file.
+ */
+// BEGIN-STUDENTS-TO-ADD-CODE
+
+
+// END-STUDENTS-TO-ADD-CODE
+
+
+
+/**
+ * @brief  Overall container for all directories and all files from the dependency file.
+ */
+// BEGIN-STUDENTS-TO-ADD-CODE
+
+
+// END-STUDENTS-TO-ADD-CODE
+
+
+
+/**
+ * @brief            Entry function to read the deendency data from stdin.
+ * @param root [IN]  The name of the root file (the deoendency file does not mention the root file, so, it has to be passed from outside).
+ * @return           The container of the read data from stdin. See the documentation on gcc -H for details on the dependencies, etc.
+ */
+// BEGIN-STUDENTS-TO-ADD-CODE
+
+
+// END-STUDENTS-TO-ADD-CODE
+
+
+
+// end of include guard
+// BEGIN-STUDENTS-TO-ADD-CODE
+
+
+// END-STUDENTS-TO-ADD-CODE
---
+++ P04_Modularisieren_von_C_Code/show-dependencies/src/output.h
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * @brief  Provides output functions for various file formats.
+ */
+// define proper header file here, with include gaurd, etc.
+// BEGIN-STUDENTS-TO-ADD-CODE
+
+
+
+
+
+// END-STUDENTS-TO-ADD-CODE
