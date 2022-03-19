8d6c054343e00ef6f79a9f2dc6946dad17418aee
Didnt Read Readme <didnt.read.readme@example.com>
2022-03-19 17:35:46 +0100

    angefangen

--- P03_Bit_Operation_struct_typedef/bin_calculator.c
+++ P03_Bit_Operation_struct_typedef/bin_calculator.c
@@ -8,6 +8,8 @@ typedef struct {
     Students: The Expression struct should hold the two operands and
     the operation (use a char for the operation)
     */
+    int operand_1, operand_2;
+    char operation;
 } Expression;
 
 int bits_per_int() {
@@ -17,10 +19,13 @@ int bits_per_int() {
 unsigned int parse_operand(char operand_str[]) {
     unsigned int operand;
     if (operand_str[0] == '0' && operand_str[1] == 'x') {
+        // hex
         sscanf(&operand_str[2], "%x", &operand);
     } else if (operand_str[0] == '0') {
+        // octal
         sscanf(&operand_str[1], "%o", &operand);
     } else {
+        // binary
         sscanf(operand_str, "%u", &operand);
     }
     return operand;
@@ -40,6 +45,15 @@ void print_bit_operation_bin(Expression expression, unsigned int result) {
     -----------------------------------
     00000000'00000000'00000000'00000011
     */
+    printf("Bin:\n");
+    print_binary(expression.operand_1);
+    printf("\n");
+    print_binary(expression.operand_2);
+    printf(" %c", expression.operation);
+    printf("\n");
+    printf("-----------------------------------\n");
+    print_binary(result);
+    printf("\n");
 }
 
 void print_bit_operation_hex(Expression expression, unsigned int result) {
@@ -49,6 +63,7 @@ void print_bit_operation_hex(Expression expression, unsigned int result) {
     Hex:
     0x0c ^ 0x0f = 0x03
     */
+    printf("Hex:\n0x%02x %c 0x%02x = 0x%02x\n", expression.operand_1, expression.operation, expression.operand_2, result);
 }
 
 void print_bit_operation_dec(Expression expression, unsigned int result) {
@@ -58,10 +73,27 @@ void print_bit_operation_dec(Expression expression, unsigned int result) {
     Dec:
     12 ^ 15 = 3
     */
+    printf("Dec:\n%d %c %d = %d", expression.operand_1, expression.operation, expression.operand_2, result);
 }
 
 unsigned int bit_operation(Expression expression) {
     // Students: Do the actual bit operation and return the result
+    int result;
+    switch (expression.operator){
+        case '&': result = expression.operand_1 & expression.operand_2;
+        break;
+        case '|': result = expression.operand_1 | expression.operand_2;
+        break;
+        case '^': result = expression.operand_1 ^ expression.operand_2;
+        break;
+        case '<': result = expression.operand_1 << expression.operand_2;
+        break;
+        case '>': result = expression.operand_1 >> expression.operand_2;
+        break;
+        default: result = -1;
+                fprintf(stderr, "Wrong operand given!\n");
+    }
+    return result;
 }
 
 int main(){
@@ -79,7 +111,8 @@ int main(){
         operand1 = parse_operand(operand1_str);
         operand2 = parse_operand(operand2_str);
 
-        Expression expression = ... ;   // Students: Create an expression
+        // Students: Create an expression
+        Expression expression = {operand1, operand2, operation} ;
 
         unsigned int result = bit_operation(expression);
         print_bit_operation_bin(expression, result);
